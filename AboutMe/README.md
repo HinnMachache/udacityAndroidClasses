713471261ab13414884e4c5a91a67ee4464e1ee2
HinnMachache <bennyhinn253.bh@gmail.com>
2024-09-13 07:03:45 +0300

    Add Interactive Button

---
+++ AboutMe/README.md
@@ -0,0 +1,5 @@
+About me:
+Layout Classes:
+Undertanding how to use different Layouts depending on Usage
+From Basics of Linear Layour to Defining User-Defined Layouts
+Other Concepts i.e Data Binding are also covered
---
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.aboutme
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.aboutme.ui.theme.AboutMeTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.buttonSubmit).setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    fun addNickName(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_text)
+        val viewText = findViewById<TextView>(R.id.nicknameView)
+
+        viewText.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        viewText.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
